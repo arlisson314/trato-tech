099d264e3e125f72773de4085048f92f9d89ed7c
Arlisson N. Costa <92638479+arlisson314@users.noreply.github.com>
2023-02-26 06:18:07 +0000

    refactor: remove a lista categorias e faz a desestruturaão dos item que seram renderizados na função map

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,49 +1,8 @@
 import Header from 'components/Header';
 import styles from './Home.module.scss';
 import relogio from 'assets/inicial.png';
-import eletronicosThumb from 'assets/categorias/thumbnail/eletronicos.png';
-import eletronicosHeader from 'assets/categorias/header/eletronicos.png';
-import automotivoThumb from 'assets/categorias/thumbnail/automotivo.png';
-import automotivoHeader from 'assets/categorias/header/automotivo.png';
-import jogosThumb from 'assets/categorias/thumbnail/jogos.png';
-import jogosHeader from 'assets/categorias/header/jogos.png';
-import escritorioThumb from 'assets/categorias/thumbnail/escritorio.png';
-import escritorioHeader from 'assets/categorias/header/escritorio.png';
-import somThumb from 'assets/categorias/thumbnail/som.png';
-import somHeader from 'assets/categorias/header/som.png';
+import categorias from 'database';
 import { useNavigate } from 'react-router-dom';
-
-const categorias = [{
-  nome: 'Eletrônicos',
-  thumbnail: eletronicosThumb,
-  heade: eletronicosHeader,
-  id: 'eletronicos',
-  descricao: 'Aqui você encontra os melhores produtos de tecnologia'
-}, {
-  nome: 'Automotivos',
-  thumbnail: automotivoThumb,
-  header: automotivoHeader,
-  id: 'automotivos',
-  descricao: 'Aqui você encontra os melhores produtos de automotivos'
-  }, {
-  nome: 'Jogos',
-  thumbnail: jogosThumb,
-  header: jogosHeader,
-  id: 'jogos',
-  descricao: 'Aqui você encontra os melhores produtos de jogos'
-  }, {
-  nome: 'Escritório',
-  thumbnail: escritorioThumb,
-  header: escritorioHeader,
-  id: 'escritorio',
-  descricao: 'Aqui você encontra os melhores produtos de escritório'
-}, {
-  nome: 'Som e imagem',
-  thumbnail: somThumb,
-  header: somHeader,
-  id: 'som',
-  descricao: 'Aqui você encontra os melhores produtos de som e imagem'
-}]
 
 function Home() {
   const navigate = useNavigate();
@@ -60,10 +19,10 @@ function Home() {
           <h1>Categorias</h1>
         </div>
         <div className={styles['categorias-container']}>
-          {categorias.map((categoria, _index) => (
-            <div key={categoria.id} onClick={() => navigate(`/categoria/${categoria.id}`)}>
-              <img src={categoria.thumbnail} alt={categoria.nome} />
-              <h1>{categoria.nome}</h1>
+          {categorias.map(({id, nome, thumbnail}, _index) => (
+            <div key={id} onClick={() => navigate(`/categoria/${id}`)}>
+              <img src={thumbnail} alt={nome} />
+              <h1>{nome}</h1>
             </div>
           ))}
         </div>
