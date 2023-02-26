571858c5599e3a2fd8a7198a73458ee1f1fbbea5
Arlisson N. Costa <92638479+arlisson314@users.noreply.github.com>
2023-02-26 10:23:09 +0000

    feat: Adiciona o reducer itens ao store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import categoriasSlice from 'reducers/categorias';
+import itensSlice from 'reducers/itens';
 
 const initialState = configureStore({
   reducer: {
     categorias: categoriasSlice,
+    itens: itensSlice,
   },
 });
 
