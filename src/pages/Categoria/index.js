75455d73b2ab88a18867eb1e492c7b8fb9ca82e2
Arlisson N. Costa <92638479+arlisson314@users.noreply.github.com>
2023-02-27 12:49:18 +0000

    feat: adiciona o componente Item ao componente Categoria

--- src/pages/Categoria/index.js
+++ src/pages/Categoria/index.js
@@ -2,21 +2,33 @@ import Header from 'components/Header';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-// import styles from './Categoria.module.scss';
+import styles from './Categoria.module.scss';
+import Item from 'components/Item';
 
 function Categoria() {
   const { nomeCategoria } = useParams();
 
-  const {nome, descricao, thumbnail } = useSelector(state => state.categorias
-    .find(categoria => categoria.id === nomeCategoria));
+  const {categoria, itens} = useSelector(state => ({
+    categoria: state.categorias.find(categoria => categoria.id === nomeCategoria),
+    itens: state.itens.filter(product => product.categoria === nomeCategoria),
+  }));
 
   return (
     <div>
       <Header
-        titulo={nome}
-        descricao={descricao}
-        imagem={thumbnail}
+        titulo={categoria.nome}
+        descricao={categoria.descricao}
+        imagem={categoria.thumbnail}
       />
+
+      <div className={styles.itens}>
+        {itens?.map(item => (
+          <Item
+            key={item.id}
+            {...item}
+          />
+        ))};
+      </div>
     </div>
   );
 }
