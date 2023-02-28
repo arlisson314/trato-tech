2a9ba16f61cb83041960ce72cb61e60a61cf58eb
Arlisson N. Costa <92638479+arlisson314@users.noreply.github.com>
2023-02-28 22:51:29 +0000

    refactor: refatora e renomei alguns  contante carrinho

--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -5,16 +5,18 @@ import Item from 'components/Item';
 
 function Carrinho() {
   const carrinho = useSelector((state) => {
-    const carrinhoReduce = state.carrinho.reduce((itens, itemNoCarrinho) => {
-      const item = state.itens.find((item) => item.id === itemNoCarrinho.id);
-      itens.push({...item, quantidade: itemNoCarrinho.quantidade});
 
-      return itens;
+    const carrinhoReduce = state.carrinho.reduce((itensAcc, itemNoCarrinho) => {
+      const item = state.itens.find((item) => item.id === itemNoCarrinho.id);
+
+      itensAcc.push({...item, quantidade: itemNoCarrinho.quantidade});
+      
+      return itensAcc;
     }, []);
-  
+    console.log(carrinhoReduce)
+
     return carrinhoReduce
   });
-  console.log(carrinho)
 
   return (
     <div>
@@ -23,7 +25,12 @@ function Carrinho() {
         descricao='Confira podutos que você adicionou ao carrino.'
       />
       <div className={styles.carrinho}>
-        {carrinho.map(item => <Item key={item.id} {...item} />)}
+        {carrinho.map(item => (
+          <Item
+            key={item.id}
+            {...item}
+          />
+        ))}
         <div className={styles.total}>
           <strong>
             Resumo da compra
