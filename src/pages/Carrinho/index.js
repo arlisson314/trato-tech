06cdf2b8941853bdf3d03a1e7d23707c97a6b9d0
Arlisson N. Costa <arlisson314@gmail.com>
2023-03-06 19:56:04 -0300

    feat: Adiciona funcionalidade para exibir valor da compra no carrinho

--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -4,18 +4,20 @@ import { useSelector } from 'react-redux'
 import Item from 'components/Item';
 
 function Carrinho() {
-  const carrinho = useSelector((state) => {
+  const {carrinho, valor} = useSelector((state) => {
+    let valor = 0;
 
     const carrinhoReduce = state.carrinho.reduce((itensAcc, itemNoCarrinho) => {
       const item = state.itens.find((item) => item.id === itemNoCarrinho.id);
-
+      valor = (item.preco * itemNoCarrinho.quantidade);
       itensAcc.push({...item, quantidade: itemNoCarrinho.quantidade});
-      
       return itensAcc;
     }, []);
-    console.log(carrinhoReduce)
 
-    return carrinhoReduce
+    return {
+      carrinho: carrinhoReduce,
+      valor
+    }
   });
 
   return (
@@ -37,7 +39,7 @@ function Carrinho() {
             Resumo da compra
           </strong>
           <span>
-            Subtotal: <strong> R$ {0.0.toFixed(2)} </strong>
+            Subtotal: <strong> R$ {valor.toFixed(2)} </strong>
           </span>
         </div>
 
