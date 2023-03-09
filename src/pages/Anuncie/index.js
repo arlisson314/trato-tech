4cce5815e3a2a3f2ea0e047285a6ca3048b3b8a0
Arlisson N. Costa <arlisson314@gmail.com>
2023-03-09 17:50:23 -0300

    feat: Cria componente Anuncie

---
+++ src/pages/Anuncie/index.js
@@ -0,0 +1,15 @@
+// import styles from './Anuncie.module/scss'
+
+import Header from 'components/Header';
+
+
+function Anuncie() {
+  return (
+    <div>
+      <Header />
+      Arlisson é muito lindo!
+    </div>
+  );
+}
+
+export default Anuncie;
