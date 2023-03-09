8c5afc4071d4c76688d6cbce781c89c87468a171
Arlisson N. Costa <arlisson314@gmail.com>
2023-03-09 18:26:04 -0300

    feat: Cria formulario e adiciona props no header do componente Anuncie

--- src/pages/Anuncie/index.js
+++ src/pages/Anuncie/index.js
@@ -1,13 +1,32 @@
 // import styles from './Anuncie.module/scss'
 
 import Header from 'components/Header';
-
+import { useSelector } from 'react-redux';
 
 function Anuncie() {
+  const categorias = useSelector(state => state.categorias);
+  console.log(categorias)
   return (
     <div>
-      <Header />
-      Arlisson é muito lindo!
+      <Header
+        titilo='Anuncie aqui!'
+        descricao='Anuncie seu produto no Melhor Site de Compras do Brasil.'
+      />
+      <form>
+        <input type='text' placeholder='Nome do prodtuto' alt='Nome do prodtuto'/>
+        <input type='text' placeholder='Descrição do prodtuto' alt='Descrição do prodtuto'/>
+        <input type='text' placeholder='URL da imagem do prodtuto' alt='URL da imagem do prodtuto'/>
+        <select>
+          <option value=''> Selicione a categoria</option>
+          {categorias?.map(categoria => (
+            <option key={categoria.id}>
+              {categoria.nome}
+            </option>
+          ))}
+        </select>
+        <input type='number' placeholder='Preço do produto' min={0}/>
+        <button type='submit'> Cadastrar prodtuto </button>
+      </form>
     </div>
   );
 }
