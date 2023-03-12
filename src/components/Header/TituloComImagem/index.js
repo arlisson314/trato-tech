da14915ccdb880086dacf50be47fd0903607305f
Arlisson N. Costa <arlisson314@gmail.com>
2023-03-12 20:30:38 -0300

    feat: recebe  o componente  Button como children e  implementa

--- src/components/Header/TituloComImagem/index.js
+++ src/components/Header/TituloComImagem/index.js
@@ -1,11 +1,12 @@
 import styles from './TituloComImagem.module.scss'
 
-function TituloComImagem({titulo, descricao, imagem, className}) {
+function TituloComImagem({children, titulo, descricao, imagem, className}) {
   return (
     <div className={`${className} ${styles.header}`}>
       <div className={styles['header-texto']}>
         <h1>{titulo}</h1>
         <h2>{descricao}</h2>
+        {children}
       </div>
       <div className={styles['header-imagem']}>
         <img src={imagem} alt={titulo} />
--- src/components/Header/TituloSemImagem/index.js
+++ src/components/Header/TituloSemImagem/index.js
@@ -1,10 +1,12 @@
 import styles from './TituloSemImagem.module.scss'
 
-function TituloSemImagem({titulo, descricao}) {
+function TituloSemImagem({children, titulo, descricao}) {
+  console.log(children);
   return (
     <div className={styles.container}>
       <h1 className={styles.titulo}>{titulo}</h1>
-      <h2  className={styles.descricao}>{descricao}</h2>
+      <h2 className={styles.descricao}>{descricao}</h2>
+      {children}
     </div>
   )
 }
